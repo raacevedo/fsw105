953e40d32f694110e2043bd1369e05d513c3b6c0
“raacevedo“ <ralph.acevedo@bryanuniversity.edu>
2020-10-11 23:50:29 -0400

    function.js

---
+++ week3/functions/function.js
@@ -0,0 +1,36 @@
+//returns sum
+function sum(num1, num2){
+    return num1 + num2
+}
+
+var result = sum(54, 45)
+console.log(result)
+
+//returns highest of 3 numbers
+function highest(num1, num2, num3)
+{
+    var max = Math.max(num1, num2, num3);
+    return max;
+}
+
+console.log(highest(1234, 2345, 13));
+
+//returns odds or evens
+function oddsEvens(n) {
+    if(n % 2 === 0){
+      return "Even";
+    } else (n % 2 !== 0); {
+      return "Odd";
+    }
+}
+console.log(oddsEvens(7))
+
+//returns string concatenation
+function stringLength(string) {
+    if (string.length > 20) {
+    console.log(string)
+    } else if (string.length <= 20) {
+    console.log(string + string)
+    }
+}
+stringLength("mississippi")
