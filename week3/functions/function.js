328aae1cfef79b8888275834ef703258982b05b9
“raacevedo“ <ralph.acevedo@bryanuniversity.edu>
2020-10-17 22:03:41 -0400

    function.js

--- week3/functions/function.js
+++ week3/functions/function.js
@@ -1,6 +1,6 @@
 //returns sum
 function sum(num1, num2){
-    return num1 + num2
+  return num1 + num2
 }
 
 var result = sum(54, 45)
@@ -9,28 +9,30 @@ console.log(result)
 //returns highest of 3 numbers
 function highest(num1, num2, num3)
 {
-    var max = Math.max(num1, num2, num3);
-    return max;
+  var max = Math.max(num1, num2, num3);
+  return max;
 }
 
 console.log(highest(1234, 2345, 13));
 
 //returns odds or evens
 function oddsEvens(n) {
-    if(n % 2 === 0){
-      return "Even";
-    } else (n % 2 !== 0); {
-      return "Odd";
-    }
+  if(n % 2 === 0){
+    return "Even";
+  } else (n % 2 !== 0); {
+    return "Odd";
+  }
 }
 console.log(oddsEvens(7))
 
 //returns string concatenation
 function stringLength(string) {
-    if (string.length > 20) {
-    console.log(string)
-    } else if (string.length <= 20) {
-    console.log(string + string)
-    }
+  if (string.length > 20) {
+    var halfStr = string.slice( 0, string.length / 2 )
+    console.log(halfStr)
+  
+  } else if (string.length <= 20) {
+  console.log(string + string)
+  }
 }
-stringLength("mississippi")
+stringLength("i was born in puerto rico")
--- week4/nodecalc.js/nodecalc.js
+++ week4/nodecalc.js/nodecalc.js
@@ -4,22 +4,22 @@ var firstNum = readlineSync.questionInt("Please enter the first number: ");
 var secondNum = readlineSync.questionInt("Please enter the second number: ");
 var selectedOperator = readlineSync.question("What operation would you like to perform: add/sub/mult/div/quit? ");
 
-function addTwoNums(num1, num2){
+function addTwoNumbers(num1, num2){
     return "Sum of number 1 and number2 = " + (num1 + num2);
 }
 
-function subtractTwoNums(num1, num2){
+function subtractTwoNumbers(num1, num2){
     return "Difference of number 1 and number2 = " + (num1 - num2);
 }
 
-function multiplyTwoNums(num1, num2){
+function multiplyTwoNumbers(num1, num2){
     return "Product of number 1 and number2 = " + (num1 * num2);
 }
 
-function divideTwoNums(num1, num2){
+function divideTwoNumbers(num1, num2){
     return "Quotient of number 1 and number2 = " + (num1 / num2);
 }
-
+ 
 if (selectedOperator == "add") {
     console.log(addTwoNumbers(firstNum, secondNum))
 }else if (selectedOperator == "sub"){
